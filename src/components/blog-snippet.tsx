f85adbdb42ae5690376bc315382f8cb1d98e61d3
erratinsilentio <kacper.kondracki@wp.pl>
2023-10-16 22:28:38 +0200

    working on the blog snippet

--- src/components/blog-snippet.tsx
+++ src/components/blog-snippet.tsx
@@ -1,24 +1,40 @@
-import { FC, useEffect, useState } from "react";
-import { getCollection, getEntry } from "astro:content";
+import React, { FC, useEffect, useState } from "react";
+import { getCollection } from "astro:content";
 
 interface BlogSnippetProps {
   count: number;
 }
 
-const BlogSnippet: FC<BlogSnippetProps> = (number) => {
-  const [posts, setPosts] = useState<unknown>(null);
+const BlogSnippet: FC<BlogSnippetProps> = ({ count = 3 }) => {
+  const [posts, setPosts] = useState<any[]>([]); // Adjust the type based on the structure of your data
 
   useEffect(() => {
     const fetchBlogs = async () => {
-      const blogPosts = await getCollection("blog");
-      console.log(blogPosts);
-      setPosts(blogPosts);
+      try {
+        const blogPosts = await getCollection("blog");
+        console.log(blogPosts[0]);
+        setPosts(blogPosts);
+      } catch (error) {
+        console.error("Error fetching blog posts:", error);
+      }
     };
 
     fetchBlogs();
   }, []);
 
-  return <div>blog-snippet</div>;
+  return (
+    <section>
+      {posts.slice(0, count).map((post, index) => {
+        console.log(post.id);
+        return (
+          <article key={index}>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.date.toDateString()}</p>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default BlogSnippet;
